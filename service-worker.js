6a20c0f290a6e92d81706ef2cbab33eb85cf2388
jemiiatsin <109674156+jemiiatsin@users.noreply.github.com>
2025-08-06 22:58:24 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+?? service-worker.js
+
+js
+const CACHE_NAME = "user-form-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
